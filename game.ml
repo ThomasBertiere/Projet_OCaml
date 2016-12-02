213cc2927bf7c3541d837f2d3bedd9510899307e
ThomasBertiere <thomas.bertiere@gmail.com>
2016-12-02 18:16:40 +0100

    modify game.ml

--- game.ml
+++ game.ml
@@ -2,36 +2,73 @@ open Gamebase
 
 (* These types are abstract in game.mli *)
 
-type state = int * player
+type state = { mutable cards_P1: List int*Player ;
+					mutable cards_P2: List int*Player ;
+					mutable pts_P1: int*Player ;
+					mutable pts_P2: int*Player ;
+					mutable p : Player ;
+					mutable end_of_round : int ; }
 
-type move = int
+type move = int (*joue une carte, donc un int*)
 
 type result = Win of player
 
+
 (* Printers *)
+(*A modifier*)
 let state2s (n,p) = Printf.sprintf "Current = %d  // %s to play" n (player2s p)
 
-let move2s n = Printf.sprintf " +%d" n
+let move2s n = Printf.sprintf " Joue : %d" n
 
 let result2s (Win p) = (player2s p) ^ " wins"
+
 
 (* Reader *)
 let readmove s = try Some (int_of_string s) with _ -> None
 
 (* You have to provide these. *)
-let initial = (0,Comput) ;;
+(* faire un truc random*)
+let initial =  ;;
 
-let turn s = let (a,b) = s in b ;;  
+(*en cours*)
+let turn s = match s with 
+	| { end_of_round=1} -> { s with end_of_round=0}  
+	| { end_of_round=0} -> { s with p = next (p), end_of_round=1}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(*vérifier que m est dans la liste de s et score<=13*)
 let is_valid s m = 
   let (a,b) = s in 
     if 0<=m && m<=3 && a<20 then true else false  ;; 
 
+(*supprime m de la liste de s et change le joueur *)
 let play s m = if is_valid s m then 
     let (a,b) = s in (a+m, (next b)) else
     failwith "move not possible";;
 
+(*retourne la liste de s*)
 let all_moves s = [1;2;3];;
+
 
 let result s = let (a,b) = s in
     if a>=20 then Some (Win b) else None ;;
