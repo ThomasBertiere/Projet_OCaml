cec8ba1761d5d3a8a1624377d05c98c2ac8b54e7
Guillaume <guillaume.de.brito12@gmail.com>
2016-12-07 10:47:53 +0100

    Work in progress 2

--- game.ml
+++ game.ml
@@ -4,8 +4,8 @@ open Gamebase
 
 type state = { cards_P1: List int*Player ;
                cards_P2: List int*Player ;
-			   played_card_P1: int*Player;
-			   played_card_P2: int*Player;
+	       played_card_P1: int*Player;
+	       played_card_P2: int*Player;
                pts_P1: int*Player        ;
                pts_P2: int*Player        ;
                p : Player                ;
@@ -34,10 +34,10 @@ let readmove s = try Some (int_of_string s) with _ -> None
 let initial =  ;;
 
 
-let turn state = match s with 
+let turn state = match state with 
   | { end_of_round=1} -> state.p 
   | { end_of_round=0} -> next (state.p)
-  | {_} -> failwith "Error turn"
+  | _ -> failwith "Error turn"
 
 
 
@@ -60,31 +60,60 @@ let is_valid s m =
 let play s m = 
 	if is_valid s m then 
 
+		(*Remove a card from the P1 or P2 cards*)
 		let rec remove x acc = function 
 			| [] -> acc		
 			| [h|tl] -> (if x=h then 
 							remove x acc::tl tl
 						else 
 							remove x acc::h::tl tl
-						)
+				     )
 		in
 
+		(*Get cards P1 and P2*)
 		let (liste_card_p1,p1)=s.cards_P1 in 
 	  	let (liste_card_p2,p2)=s.cards_P2 in
+		(*Get the last card played by P1 or P2*)
 		let (played_card_p1,a)=s.played_card_P1 in 
+		let (played_card_p2,a)=s.played_card_P2 in 
+		(*Get scores*)
+		let (score_P1,p1)=s.pts_P1 in
+		let (score_P2,p2)=s.pts_P2 in 
+		(*Get the cards of the player P*)
+		let list_pl = if p1=s.p then liste_card_p1 else liste_card_p2 in
+		(*Get the score of the player P*)
+		let score_pl = if p1=s.p then score_P1 else score_P2 in 
 
 
-	  	let list_pl = if p1=s.p then liste_card_p1 else liste_card_p2 in 
-	    let new_cards = ((remove m [] list_pl),s.p) in 
+
+ 		(*Remove the card played*)
+    	let new_cards = ((remove m [] list_pl),s.p) in 
+		(*Card played*)
 		let played_card = (m,s.p) in
-		
-		let eor = if s.end_of_round=1 then 0 else 1 in 
-		let new_player = turn s in 
 
-		if s.end_of_round=1 then 
-			let (played_card_p1,a)=s.played_card_P1 in 
-				if played_card>played_card_p1 then 
-				
+		let eor = if s.end_of_round=1 then 0 else 1 in 
+		(*Get the next player*)
+		let next_player = turn s in
+		
+		
+		if p1=s.p then 
+			(if s.end_of_round=1 then 
+				(if played_card>played_card_p1 then 
+					let new_score_P1=score_P1+1 in
+					{cards_P1=new_cards;cards_P2=liste_card_p2;played_card_P1=played_card;played_card_P2=played_card_p2;
+					pts_P1=new_score_P1;pts_P2=score_P2;p=next_player;end_of_round=eor}
+				else
+					let new_score_P2=score_P2+1 in
+					{cards_P1=new_cards;cards_P2=liste_card_p2;played_card_P1=played_card;played_card_P2=played_card_p2;
+					pts_P1=score_P1;pts_P2=new_score_P2;p=next_player;end_of_round=eor}
+				)
+			else
+				{cards_P1=new_cards;cards_P2=liste_card_p2;played_card_P1=played_card;played_card_P2=played_card_p2;
+				pts_P1=score_P1;pts_P2=score_P2;p=next_player;end_of_round=eor}
+			)	
+		else
+			
+			
 
 
 	else
