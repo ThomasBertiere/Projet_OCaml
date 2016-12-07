b2ee67f1f5a98107be793b283fed9c16c9ab51ee
ThomasBertiere <thomas.bertiere@gmail.com>
2016-12-07 15:15:26 +0100

    modif de test

--- test.ml
+++ test.ml
@@ -110,17 +110,58 @@ let result2s = function
   | Egality -> "Egality"
 
 
-let random_card remaning_card =
-  let rec selected_card card_list i = 
-    if i = 0 then List.hd card_list else selected_card (List.tl card_list) (i-1) 
-  in 
-  let (*rand1 donne l'indice*) in
-  let rand2
-        if (selected_card card_list le_rand)!=-1 then 
+let rec random_card = function 
+  | [] -> [],[]
+  | hd::tl -> 
+      let (p1,p2)=random_card tl in
+        if (Random.int 2)= 0 then 
+          if (List.length p1)<27 then 
+            (hd::p1),p2 
+          else 
+            p1,(hd::p2) 
+        else
+        if (List.length p2)<27 then 
+          p1,(hd::p2)
+        else 
+          (hd::p1),p2 
+;;
 
 let initial =  
-  random_card [1;1;1;1;2;2;2;2;3;3;3;3;4;4;4;4;5;5;5;5;6;6;6;6;7;7;7;7;8;8;8;8;9;9;9;9;10;10;10;10;11;11;11;11;12;12;12;12;13;13;13;13];;
+  Random.init (567727889) ;
+  let card_p1,card_p2=random_card [1;1;1;1;2;2;2;2;3;3;3;3;4;4;4;4;5;5;5;5;6;6;6;6;7;7;7;7;8;8;8;8;9;9;9;9;10;10;10;10;11;11;11;11;12;12;12;12;13;13;13;13] in
+  let p1= if (Random.int 2)= 0 then Human else Comput in 
+  let p2= next p1 in 
+    { cards_P1=card_p1,p1  ;
+      cards_P2=card_p2,p2  ;
+      played_card_P1=0,p1  ;
+      played_card_P2=0,p2  ;
+      pts_P1= 0,p1         ;
+      pts_P2= 0,p2         ;
+      p= p1                ; 
+      end_of_round=0       ; 
+    };;
 
+
+
+let turn state = match state with 
+  | { end_of_round=1} -> state.p 
+  | { end_of_round=0} -> next (state.p)
+  | _ -> failwith "Error turn"
+
+
+
+let is_valid s m =
+  let rec member x = function 
+    | [] -> false 
+    | h::tl -> if h=x then true else member x tl 
+  in 
+  let (liste_card_p1,p1)=s.cards_P1 in 
+  let (liste_card_p2,p2)=s.cards_P2 in
+  let list_pl = if p1=s.p then liste_card_p1 else liste_card_p2 in
+  let in_hand=member m list_pl in 
+  let sc_p1,a=s.pts_P1 in
+  let sc_p2,a=s.pts_P2 in 
+    (not(liste_card_p1=[] && liste_card_p2=[])) && sc_p2<=13 && sc_p1<=13 && in_hand
 
 (* ########################### TEST #####################*)
 
@@ -135,15 +176,45 @@ let state_test={
   end_of_round=1                           ; 
 };;
 
+let state_test2={
+  cards_P1=[1;2;3;4;5;6;7;8;9],Human       ;
+  cards_P2=[3;4;6;8;9;10;11;3;2],Comput    ;
+  played_card_P1=2,Human                   ;
+  played_card_P2=3,Comput                  ;
+  pts_P1=14,Human                          ;
+  pts_P2=10,Comput                         ;
+  p=Human                                  ; 
+  end_of_round=1                           ; 
+};;
+
+let state_test3={
+  cards_P1=[1;2;3;4;5;6;7;8;9],Human       ;
+  cards_P2=[],Comput                       ;
+  played_card_P1=2,Human                   ;
+  played_card_P2=3,Comput                  ;
+  pts_P1=14,Human                          ;
+  pts_P2=10,Comput                         ;
+  p=Comput                                 ; 
+  end_of_round=1                           ; 
+};;
+
+(* Print a state *)
 Printf.printf "%s%!\n" (state2s state_test);;      (*########### OK ############*)
+(* Print a move *)
 Printf.printf "%s%!\n" (move2s 3);;                (*########### OK ############*)
+(* Print a result *)
 Printf.printf "%s%!\n" (result2s (Win (Human)));;  (*########### OK ############*)
 Printf.printf "%s%!\n" (result2s Egality);;        (*########### OK ############*)
-
-
-
-
-
+(* initialization test *)
+Printf.printf "%s%!\n" (state2s initial);;         (*########### OK ############*)
+(* turn test *)
+Printf.printf "%s%!\n" (player2s (turn initial));; (*########### OK ############*)
+Printf.printf "%s%!\n" (player2s (turn state_test));;(*######### OK ############*)
+(* is_valid test *)
+is_valid state_test 1;;                            (*########### OK ############*)
+is_valid state_test 10;;                           (*########### OK ############*)
+is_valid state_test2 1;;                           (*########### OK ############*)
+is_valid state_test3 1;;                           (*########### OK ############*)
 
 
 
